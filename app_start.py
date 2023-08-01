f1e14ee3eed05848278ee373946eabccf3043725
ai4he <106041678+ai4he@users.noreply.github.com>
2023-08-01 10:10:57 -0400

    Update app_start.py

--- app_start.py
+++ app_start.py
@@ -445,7 +445,8 @@ preset = 'start.json'
 prompt_file = preset
 # openai_model = "gpt-3.5-turbo-16k"
 # openai_model = "gpt-3.5-turbo"
-openai_model = "gpt-4"
+# openai_model = "gpt-4"
+openai_model = "gpt-4-0314"
 sessions = {}
 max_iterations = 2
 
